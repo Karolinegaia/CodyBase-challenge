c383fdedecec270be0c356102fb039c4b67f7c3e
Karoline Gaia <drakarolinegaia@gmail.com>
2023-02-10 22:39:24 -0300

    Creating the footer component

---
+++ pokedex/src/components/Footer.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class='text'>
+        <p class='name'>Karoline Gaia. Todos os Direitos Reservados</p>       
+    </div>       
+</template>
+<style src="../style/FooterStyle.sass" lang="sass"></style>
