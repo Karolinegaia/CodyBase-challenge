4570114158997df7beb1c8028ccb4e8a1b90248a
Karoline Gaia <drakarolinegaia@gmail.com>
2023-02-10 22:30:31 -0300

    Creating the header component

---
+++ pokedex/src/components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="title">
+    <img src="../../public/pokedex.png" alt="pokedex" />
+    <h1>Pokedex</h1>
+  </div>
+</template>
+<script>
+
+</script>
+<style src="../style/HeaderStyle.sass" lang="sass" />
